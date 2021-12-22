5216cbd895b034c6bc01313948a696cb4f5d24d9
aleeeexxx <499365592@qq.com>
2021-12-21 16:49:32 -0800

    new empirical method

--- gen_traj_1.py
+++ gen_traj_1.py
@@ -8,34 +8,21 @@ from scipy.optimize import least_squares
 # import matplotlib.pyplot as plt
 # import scipy.linalg.hadamard as hadamard
 
-def get_mc_traj(n_traj=3,n_state=3,len=10):
-    p = np.zeros([n_traj,n_state,n_state])
-    p_gt = np.zeros([n_traj,n_state,n_state])
-    
-    for k in range(n_traj):
-        for i in range(n_state):
-            for j in range(n_state):
-                temp = np.random.rand()
-                if temp > 0.5:
-                    p_gt[k,i,j] = 1
-                else:
-                    p_gt[k,i,j] = 0
-                if j == 0:
-                    p[k,i,j] = p_gt[k,i,j]
-                else:
-                    p[k,i,j] = p[k,i,j-1] + p_gt[k,i,j]
-            # print(" p[k,i,:]", p[k,i,:])   
-            # print("p[k,i,n_state-1]", p[k,i,n_state-1])
-            # if p[k,i,n_state-1] != 0:
-            p[k,i,:] = p[k,i,:] / p[k,i,n_state-1]
-            # print(" p[k,i,:]", p[k,i,:])   
-        # print("k",k) 
-        # print("p_gt",p_gt[k])   
+def get_gt(n_traj=10,n_state=3,len=10):
+    p_gt = np.random.rand(n_state,n_state)       
+    p_gt = (p_gt.T/p_gt.sum(axis=1)).T
+    p_gt = np.where(np.isnan(p_gt), 0, p_gt)
 
-        p_gt[k] = (p_gt[k].T/p_gt[k].sum(axis=1)).T
-        p_gt[k] = np.where(np.isnan(p_gt[k]), 0, p_gt[k])
-        # print("p_gt",p_gt[k])
-      
+    p = np.zeros([n_state,n_state])
+    for i in range(n_state):
+        for j in range(n_state):
+            if j == 0:
+                p[i,j] = p_gt[i,j]
+            else:
+                p[i,j] = p[i,j-1] + p_gt[i,j]
+
+
+    
     # initial state
     traj = np.zeros([n_traj,len])
     for k in range(n_traj):
@@ -43,34 +30,56 @@ def get_mc_traj(n_traj=3,n_state=3,len=10):
         traj[k,0] = s + 1
         for i in range(1,len):      
             # print("p[k]", p[k])
-            s = get_next_state(s,p[k])
+            s = get_next_state(s,p)
             traj[k,i] = s + 1
             # if s != 0:
             #     counter += 1
     #         print(counter)
-    return traj, p_gt
+    return traj , p_gt
+
+
+def get_p_k(n_traj=3,n_state=3):
+    p = np.zeros([n_traj,n_state,n_state])
+    p_k = np.zeros([n_traj,n_state,n_state])
+    
+    for k in range(n_traj):
+        for i in range(n_state):
+            for j in range(n_state):
+                temp = np.random.rand()
+                if temp > 0.5:
+                    p_k[k,i,j] = 1
+                else:
+                    p_k[k,i,j] = 0
+                if j == 0:
+                    p[k,i,j] = p_k[k,i,j]
+                else:
+                    p[k,i,j] = p[k,i,j-1] + p_k[k,i,j]
+
+            p[k,i,:] = p[k,i,:] / p[k,i,n_state-1] 
+
+        p_k[k] = (p_k[k].T/p_k[k].sum(axis=1)).T
+        p_k[k] = np.where(np.isnan(p_k[k]), 0, p_k[k])
+        # print("p_gt",p_gt[k])
+    return  p_k
 
 def get_next_state(i,p):
-    
     prob = np.random.rand()
-    for j in range(len(p[0])):
+    for j in range(len(p)):
         # print("p[i,j]",p[i,j])
         if prob < p[i,j]:
             return j
     return j
 
 def get_p_bar(traj, n_state):
-    total = len(traj)*(len(traj[0])-1)
+    # total = (len(traj[0])-1)
     p_bar = np.zeros([n_state,n_state])
     # print("traj[0]",len(traj))
     # print("traj[1]",len(traj[1]))
     for k in range(len(traj)):
         for i in range(len(traj[1])-1):
             p_bar[int(traj[k,i])-1,int(traj[k,i+1])-1] += 1
-    # print("p_bar",p_bar)
-    p_bar = p_bar/total
-    # print("total",total)
-    # print("p_bar",p_bar)
+    p_bar = (p_bar.T/p_bar.sum(axis=1)).T
+    p_bar = np.where(np.isnan(p_bar), 0, p_bar)
     return p_bar
 
 def get_alpha(p_bar,p_k):
@@ -79,7 +88,8 @@ def get_alpha(p_bar,p_k):
     x0 = np.array([1/n]*n)
     # print("x0",x0)
     res = least_squares(fun, x0, bounds=(0, 1),args=(p_bar, p_k), verbose=1)
-    return res.x
+    alpha = res.x/sum(res.x)
+    return alpha
 
 def model(x, p_k):
     sum = 0
@@ -90,14 +100,42 @@ def model(x, p_k):
 def fun(x,p_bar,p_k):
     return sum((model(x, p_k) - p_bar)**2)
 
+def get_p_hat(alpha,p_k):
+    sum = 0
+    for i in range(len(alpha)):
+        sum += p_k[i]*alpha[i]
+    return sum
+
+def get_error(p_hat,p_gt):
+    delta = p_hat - p_gt
+    print("delta",delta)
+    norm = np.linalg.norm(delta)
+    print("norm",norm)
+    error = delta/norm
+    # print("error",error)
+    return error
 
 
-traj, p_k = get_mc_traj(5,5,100)
-print("p_k",p_k)
+
+traj, p_gt = get_gt(n_traj=1000,n_state=5,len=3000)
+print("p_gt",p_gt)
+
+p_k = get_p_k(n_traj=300,n_state=5)
+# print("p_k",p_k)
 # print("traj",traj)
 
-p_bar = get_p_bar(traj, 5)
+# p_k[3] = p_gt
+# print("p_k",p_k)
+
+p_bar = get_p_bar(traj, n_state=5)
 print("p_bar",p_bar)
 
 alpha = get_alpha(p_bar,p_k)
-print("alpha", alpha)
+# print("alpha", alpha)
+
+p_hat = get_p_hat(alpha,p_k)
+print("p_hat",p_hat)
+
+print("p_gt",p_gt)
+error = get_error(p_hat,p_gt)
+print("error in Frobenius norm",error)
